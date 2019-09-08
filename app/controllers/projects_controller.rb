70741e404066e2fa345c81354c73b775bade8cfb
Diego Lafuente <dlaf88@gmail.com>
2019-09-08 18:44:30 -0400

    adding workflow specs to the projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,12 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new()
   end
+
+  def create
+    @workflow = CreatesProject.new(name:"")
+  end
+
+  def index
+    @projects = Project.all
+  end
 end
