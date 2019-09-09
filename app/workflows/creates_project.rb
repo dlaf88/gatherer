8ff456d0030075685750df9d439868a7d5bc999a
Diego Lafuente <dlaf88@gmail.com>
2019-09-09 18:09:46 -0400

    staged the creates project added the convert_to_string_tasks method

--- app/workflows/creates_project.rb
+++ app/workflows/creates_project.rb
@@ -1,12 +1,27 @@
 class CreatesProject
-  attr_accessor :name,:project,:tasks
+  attr_accessor :name,:project,:task_string
 
-  def initialize(name="",tasks=[])
+  def initialize(name="",task_string="")
     @name = name
+    @task_string = task_string
   end
 
   def build
     self.project = Project.new(name=self.name)
+    project.tasks = convert_string_to_tasks
+    project
+  end
+
+  def convert_string_to_tasks
+    task_string.split("\n").map do |one_task|
+      title,size = one_task.split(":")
+      Task.new(title: title, size: size_as_integer(size))
+    end
+  end
+
+  def size_as_integer(size)
+    return 1 if size.blank?
+    [size.to_i,1].max
   end
 
 end
--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -11,8 +11,10 @@ RSpec.describe CreatesProject do
 
   describe 'string parsing' do
     it "creates project with empty task string" do
-
-  
+      creator = CreatesProject.new(name: "Project Runway", task_string:"")
+      creator.build
+      creator.convert_string_to_tasks
+      expect(creator.project.tasks).to be_empty
     end
 
   end
