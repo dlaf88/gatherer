ede30cd0d08717893b9b25fb82b01b3a5e129126
Diego Lafuente <dlaf88@gmail.com>
2019-09-10 17:51:01 -0400

    added change to the creates project spec after realized that creating a project was not necessary

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -12,51 +12,44 @@ RSpec.describe CreatesProject do
   describe 'string parsing' do
     it "creates project with empty task string" do
       creator = CreatesProject.new(name: "Project Runway",task_string: "")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks).to be_empty
+      tasks = creator.convert_string_to_tasks
+      expect(tasks).to be_empty
     end
 
     it 'creates project with task string but no size' do
       creator = CreatesProject.new(name: "Project Runway",task_string: "This is the first task")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks.size).to eq(1)
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
     end
 
     it 'creates project with task string with arbitrary size' do
       creator = CreatesProject.new(name: "Project Runway",task_string: "This is the first task:2")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks.first).to have_attributes(title: "This is the first task", size: 2)
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.first).to have_attributes(title: "This is the first task", size: 2)
     end
 
     it 'creates project with task string with size zero' do
       creator = CreatesProject.new(name: "Project Runway",task_string: "This is the first task:0")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks.first).to have_attributes(title: "This is the first task", size: 1)
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.first).to have_attributes(title: "This is the first task", size: 1)
     end
 
     it 'creates project with task string with malformed size' do
       creator = CreatesProject.new(name: "Project Runway",task_string: "This is the first task:two")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks.first).to have_attributes(title: "This is the first task", size: 1)
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.first).to have_attributes(title: "This is the first task", size: 1)
     end
 
     it 'creates project with task string with negative size' do
       creator = CreatesProject.new(name: "Project Runway",task_string: "This is the first task:-1")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks.first).to have_attributes(title: "This is the first task", size: 1)
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.first).to have_attributes(title: "This is the first task", size: 1)
     end
 
     it "handles multiple tasks" do
       creator = CreatesProject.new(name: "",task_string: "First Task:2\nSecond task:3")
-      creator.build
-      creator.convert_string_to_tasks
-      expect(creator.project.tasks).to match([an_object_having_attributes(title: "First Task",size: 2),an_object_having_attributes(title: "Second task",size: 3)])
+      tasks = creator.convert_string_to_tasks
+      expect(tasks).to match([an_object_having_attributes(title: "First Task",size: 2),an_object_having_attributes(title: "Second task",size: 3)])
     end
 
 
